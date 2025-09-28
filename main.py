38c1ec2e976c01dfec12f9bf417fa7032c776739
Er. Niranjan Kasote <kasoteniranjan23@gmail.com>
2025-09-28 22:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI, status, responses, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from pydantic import BaseModel
 from uuid import UUID
 
 
 app = FastAPI()
+
+
+
+# Allow frontend origin
+origins = [
+    "http://127.0.0.1:5500",  # your local frontend
+    "http://localhost:5500",  # sometimes browser uses localhost
+    "https://your-frontend-domain.com"  # (later when deployed)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     # allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],       # allow all HTTP methods (POST, GET, etc.)
+    allow_headers=["*"],       # allow all headers
+)
+
 
 
 @app.get('/', deprecated=False)
